7280ef3d3ff477fba4cfa4f8e9a21410b691790d
mubin25j <mubin25j@gmail.com>
2025-08-14 01:02:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  mobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+});
